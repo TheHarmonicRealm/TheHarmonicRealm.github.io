2bf99927ea7565fcfd85f2ab1c1106783f596da9
Noah <44441682+TheHarmonicRealm@users.noreply.github.com>
2020-02-04 13:19:51 -0800

    Update CurrentSchedule.js

--- JS/CurrentSchedule.js
+++ JS/CurrentSchedule.js
@@ -1,5 +1,6 @@
-function goToCurrentDay(currentPage)
+function goToCurrentDay(currentPage) {
 var d = new Date();
 var today = d.getDay();
 var destination = currentPage+"#"+today;
 document.location.replace(destination);
+}
