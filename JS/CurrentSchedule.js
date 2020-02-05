ff83054765f890448a60ecf7ab0762e5bcba980e
Noah Solomon <crashtest64@micahsolomon>
2020-02-05 15:46:47 -0800

    simpler function, ignore .DS_Store crap

--- JS/CurrentSchedule.js
+++ JS/CurrentSchedule.js
@@ -1,6 +1,7 @@
-function goToCurrentDay(currentPage) {
+function goToCurrentDay() {
 var d = new Date();
 var today = d.getDay();
+var currentPage = window.location;
 var destination = currentPage+"#"+today;
 document.location.replace(destination);
 }
