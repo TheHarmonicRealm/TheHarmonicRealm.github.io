1f1b2dccecdf4dcfd00e843910d8f4d355d6ed48
Noah Solomon <crashtest64@micahsolomon>
2020-02-07 20:25:05 -0800

    moved to new system for some pages

--- JS/LoadLinks.js
+++ JS/LoadLinks.js
@@ -1,4 +1,4 @@
 function loadLinks() {
     document.getElementById("links").innerHTML = 
-    "<a href=\"AdvisorySchedule.html\">Advisory Schedule</a> <br> <a href=\"LateStartSchedule.html\">Late Start Schedule</a> <br>";
+    "<a href=\"index.html\">Regular Schedules</a> <br> <a href=\"AdvisorySchedule.html\">Advisory Schedule</a> <br> <a href=\"LateStartSchedule.html\">Late Start Schedule</a> <br> <a href=\"FinalsSchedule.html\">Finals Schedule</a> <br>";
 }
