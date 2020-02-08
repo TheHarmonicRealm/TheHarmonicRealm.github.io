4e3559e6e2b7843a2f13f46c11497f954c37013c
Noah Solomon <crashtest64@micahsolomon>
2020-02-07 17:30:07 -0800

    relative links

--- JS/LoadLinks.js
+++ JS/LoadLinks.js
@@ -1,4 +1,4 @@
 function loadLinks() {
     document.getElementById("links").innerHTML = 
-    "<a href=\"https://TheHarmonicRealm.github.io/AdvisorySchedule.html\">Advisory Schedule</a> <br> <a href=\"https://TheHarmonicRealm.github.io/LateStartSchedule.html\">Late Start Schedule</a> <br>";
+    "<a href=\"AdvisorySchedule.html\">Advisory Schedule</a> <br> <a href=\"LateStartSchedule.html\">Late Start Schedule</a> <br>";
 }
