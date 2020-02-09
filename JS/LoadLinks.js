4dabcefe5e809577978086e9e471b1e4f61641ec
Noah Solomon <crashtest64@micahsolomon>
2020-02-08 16:49:11 -0800

    commit before everything moves, and some things break

--- JS/LoadLinks.js
+++
@@ -1,4 +0,0 @@
-function loadLinks() {
-    document.getElementById("links").innerHTML = 
-    "<a href=\"index.html\">Regular Schedules</a> <br> <a href=\"AdvisorySchedule.html\">Advisory Schedule</a> <br> <a href=\"LateStartSchedule.html\">Late Start Schedule</a> <br> <a href=\"FinalsSchedule.html\">Finals Schedule</a> <br>";
-}
---
+++ JS/LoadScheduleLinks.js
@@ -0,0 +1,4 @@
+function loadLinks() {
+    document.getElementById("links").innerHTML = 
+    "<a href=\"RegularSchedule.html\">Regular Schedule</a> <br> <a href=\"AdvisorySchedule.html\">Advisory Schedule</a> <br> <a href=\"LateStartSchedule.html\">Late Start Schedule</a> <br> <a href=\"FinalsSchedule.html\">Finals Schedule</a> <br>";
+}
