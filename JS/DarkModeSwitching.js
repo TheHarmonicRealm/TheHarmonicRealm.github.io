6399e0f902b68f9d33a1d289b44c028a0598b3ed
Noah Solomon <crashtest64@micahsolomon>
2020-02-26 12:19:22 -0800

    done! Next: styling the button

--- JS/DarkModeSwitching.js
+++ JS/DarkModeSwitching.js
@@ -11,7 +11,59 @@ function setButtonText() {
 
 }
 
+/* takes the variable name without the hyphens and a string "dark" or "light", then adds that to the front */
+function variableThemeSwitch(variable, mode) {
+    var realVariableName = "--" + variable;
+    var newVariableName = "--" + mode + "-" + variable;
+
+    document.documentElement.style.setProperty(realVariableName, "var(" + newVariableName + ")");
+
+}
+
+function setColors() {
+    if (darkModeEnabled) {
+        //set it to be dark
+        variableThemeSwitch("inactive", "dark");
+        variableThemeSwitch("hover", "dark");
+        variableThemeSwitch("active", "dark");
+        variableThemeSwitch("inactive-underline", "dark");
+        variableThemeSwitch("hover-underline", "dark");
+        variableThemeSwitch("active-underline", "dark");
+        variableThemeSwitch("link-underline-width", "dark");
+        variableThemeSwitch("background-color", "dark");
+        variableThemeSwitch("body-color", "dark");
+        variableThemeSwitch("links-background", "dark");
+        variableThemeSwitch("title-color", "dark");
+        variableThemeSwitch("title-background", "dark");
+        variableThemeSwitch("underline-color", "dark");
+        variableThemeSwitch("gradient-top", "dark");
+        variableThemeSwitch("gradient-bottom", "dark");
+    }
+    else if (!darkModeEnabled) {
+        //set it to be light
+        variableThemeSwitch("inactive", "light");
+        variableThemeSwitch("hover", "light");
+        variableThemeSwitch("active", "light");
+        variableThemeSwitch("inactive-underline", "light");
+        variableThemeSwitch("hover-underline", "light");
+        variableThemeSwitch("active-underline", "light");
+        variableThemeSwitch("link-underline-width", "light");
+        variableThemeSwitch("background-color", "light");
+        variableThemeSwitch("body-color", "light");
+        variableThemeSwitch("links-background", "light");
+        variableThemeSwitch("title-color", "light");
+        variableThemeSwitch("title-background", "light");
+        variableThemeSwitch("underline-color", "light");
+        variableThemeSwitch("gradient-top", "light");
+        variableThemeSwitch("gradient-bottom", "light");
+    }
+
+}
+
 function darkModeSwitch() {
     darkModeEnabled = !darkModeEnabled
+
+    setColors();
+
     setButtonText();
 }
