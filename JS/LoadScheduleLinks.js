1b29346e61666f1f2a6f4be820c2c27b86c50b15
Noah Solomon <crashtest64@micahsolomon>
2020-02-26 20:02:53 -0800

    sticky top bar, helped (read: written almost entirely) by Henry (username Elkwizard)

--- JS/LoadScheduleLinks.js
+++ JS/LoadScheduleLinks.js
@@ -1,4 +1,4 @@
 function loadLinks() {
     document.getElementById("links").innerHTML = 
-    "<a href=\"/Schedules/RegularSchedule.html\">Regular Schedule</a> <br> <a href=\"/Schedules/AdvisorySchedule.html\">Advisory Schedule</a> <br> <a href=\"/Schedules/FinalsSchedule.html\">Finals Schedule</a> <br>";
+    "<a href=\"/Schedules/RegularSchedule.html\">Regular Schedule</a> <a href=\"/Schedules/AdvisorySchedule.html\">Advisory Schedule</a> <a href=\"/Schedules/FinalsSchedule.html\">Finals Schedule</a>";
 }
