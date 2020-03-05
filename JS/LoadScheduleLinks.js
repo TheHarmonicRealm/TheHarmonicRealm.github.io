bb33b746437d8d7dee226d7ea61ea9e5b1405ebc
Noah <44441682+TheHarmonicRealm@users.noreply.github.com>
2020-03-05 14:26:25 -0800

    Pages are moving

--- JS/LoadScheduleLinks.js
+++ JS/LoadScheduleLinks.js
@@ -1,4 +1,4 @@
 function loadLinks() {
     document.getElementById("links").innerHTML = 
-    "<a id=\"regular\" href=\"/Schedules/RegularSchedule.html\">Regular Schedule</a> <a id=\"advisory\" href=\"/Schedules/AdvisorySchedule.html\">Advisory Schedule</a> <a id=\"finals\" href=\"/Schedules/FinalsSchedule.html\">Finals Schedule</a>";
+    "<a id=\"regular\" href=\"/Schedules/Regular.html\">Regular Schedule</a> <a id=\"advisory\" href=\"/Schedules/Advisory.html\">Advisory Schedule</a> <a id=\"finals\" href=\"/Schedules/Finals.html\">Finals Schedule</a>";
 }
