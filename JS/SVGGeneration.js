3992b2e49b1d59d5e665a969c14692b6c7ed61c4
Noah <44441682+TheHarmonicRealm@users.noreply.github.com>
2020-08-03 23:12:35 -0700

    done, now the SVG can be downloaded

--- JS/SVGGeneration.js
+++ JS/SVGGeneration.js
@@ -1,4 +1,20 @@
 var completeSVG;
+var downloadLink;
+
 function generateContentOfFile() {
     completeSVG = `<?xml version="1.0" standalone="no"?> <svg version="1.1" baseProfile="full" width="${widthInput}" height="${heightInput}" xmlns="http://www.w3.org/2000/svg">` + "\n" + svgInput + "\n" + "</svg>";
 } 
+
+//FROM https://ourcodeworld.com/articles/read/189/how-to-create-a-file-and-generate-a-download-with-javascript-in-the-browser-without-a-server
+function downloadSVG(filename, text) {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+}
--- JS/SVGMain.js
+++ JS/SVGMain.js
@@ -2,11 +2,14 @@ var svgInput;
 var widthInput;
 var heightInput;
 var filenameInput;
+var fullFilename;
 
 function setValues() {
     svgInput = document.getElementById("main-input").value;
     widthInput = document.getElementById("width-input").value;
     heightInput = document.getElementById("height-input").value;
+    filenameInput = document.getElementById("filename-input").value;
+    fullFilename = filenameInput + ".svg";
 }
 function renderSVG() {
     document.getElementById("svg-display").innerHTML = svgInput;
@@ -15,19 +18,3 @@ function renderSVG() {
 }
 
 
-/*
-
-//FROM https://ourcodeworld.com/articles/read/189/how-to-create-a-file-and-generate-a-download-with-javascript-in-the-browser-without-a-server
-function downloadSVG() {
-    var element = document.getElementById("download-svg");
-    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-    element.setAttribute('download', filenameInput);
-
-    element.style.display = 'none';
-    document.body.appendChild(element);
-
-    element.click();
-
-    document.body.removeChild(element);
-}
-*/
