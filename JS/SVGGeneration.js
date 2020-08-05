2722cf39c2c541b0eca730c9dc41185de9cb321f
Noah <44441682+TheHarmonicRealm@users.noreply.github.com>
2020-08-04 19:14:00 -0700

    set viewBox in download

--- JS/SVGGeneration.js
+++ JS/SVGGeneration.js
@@ -2,7 +2,7 @@ var completeSVG;
 var downloadLink;
 
 function generateContentOfFile() {
-    completeSVG = `<?xml version="1.0" standalone="no"?>\n <svg version="1.1" baseProfile="full" width="${widthInput}" height="${heightInput}" xmlns="http://www.w3.org/2000/svg">` + "\n" + svgInput + "\n" + "</svg>";
+    completeSVG = `<?xml version="1.0" standalone="no"?>\n <svg version="1.1" baseProfile="full" width="${widthInput}" height="${heightInput}" viewBox = "0, 0, ${widthInput}, ${heightInput}" xmlns="http://www.w3.org/2000/svg">` + "\n" + svgInput + "\n" + "</svg>";
 } 
 
 //FROM https://ourcodeworld.com/articles/read/189/how-to-create-a-file-and-generate-a-download-with-javascript-in-the-browser-without-a-server
