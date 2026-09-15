f1ac5e27b07a4d26f8be88dd3be00de2a167cc97
Noah <44441682+TheHarmonicRealm@users.noreply.github.com>
2026-09-14 19:57:24 -0700

    Make navbar script more backwards-compatible (this is all just for fun)

--- assets/js/navbar_spacing.js
+++ assets/js/navbar_spacing.js
@@ -2,16 +2,33 @@ const navbar = document.querySelector("#navbar");
 const navbar_spacer = document.querySelector("#navbar-spacer");
 
 function matchSpacerHeight() {
-    let navbar_height = navbar.getBoundingClientRect().height;
+    var navbar_height = navbar.getBoundingClientRect().height;
     navbar_spacer.style.height = `${navbar_height}px`;
     // set CSS variable for anchor adjustments
     document.documentElement.style.setProperty("--navbar-height", `${navbar_height}px`);
 }
 
-const observer = new ResizeObserver(matchSpacerHeight);
-observer.observe(navbar);
+var started_resizing = false;
 
-// only float if user has JS
-navbar.style.position = "fixed";
+try {
+    const observer = new ResizeObserver(matchSpacerHeight);
+    observer.observe(navbar);
+    started_resizing = true;
+}
+catch (e) { // optional catch elements are only from ~2018
+    try {
+        console.log("You have an old browser! Falling back from ResizeObserver");
+        window.addEventListener("resize", matchSpacerHeight);
+        started_resizing = true;
+    }
+    catch (e) {
+        console.log("Didn't work either. No floating navbar for you.")
+    }
+}
 
-matchSpacerHeight();
+if (started_resizing) {
+    // only float if user has JS
+    navbar.style.position = "fixed";
+    // also don't do the onload sizing of spacer -- keep it at 0
+    matchSpacerHeight();
+}
