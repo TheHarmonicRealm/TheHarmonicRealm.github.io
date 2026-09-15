dcee3d2270ed08125e493147efb22ace9c6390bf
Noah <44441682+TheHarmonicRealm@users.noreply.github.com>
2026-09-15 11:26:59 -0700

    Script improvements (redundancy, naming)

--- assets/js/video_button.js
+++ assets/js/video_button.js
@@ -2,12 +2,6 @@ let playButton = document.getElementById("play-checkbox");
 
 playButton.addEventListener("input", function(){handlePlayButton(this.checked)});
 
-let all_videos = Array.from(document.getElementsByClassName("looping-video"));
-
-all_videos.forEach(element => {
-    element.addEventListener("click", function(){element.paused ? element.play() : element.pause()});
-});
-
 function handlePlayButton(play) {
     if (play) {
         console.log("starting");
